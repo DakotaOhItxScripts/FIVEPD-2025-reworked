fe99834df187cfa49c39fe39879689e25e4d4975
DakotaScripts <alexmahomebcso.org@gmail.com>
2025-09-06 13:15:30 -0500

    Add files via upload

---
+++ [sandymap]/cfx_gn_sheriff_sandyshores/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'G&N_s Studio'
+description 'Sheriff Department - Sandy Shores'
+version '4.0.0'
+
+this_is_a_map 'yes'
+
+dependencies {
+    '/gameBuild:2189',
+    'cfx_gn_sheriff_dlc'
+}
+
+escrow_ignore {
+    'stream/**/*.ytd',
+    'stream/ydr/gn_sheriff_sandy_detail_b.ydr'
+}
+
+dependency '/assetpacks'
