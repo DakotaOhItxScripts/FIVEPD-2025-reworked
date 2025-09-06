24d6d46a03742bf785c695319fe27538c791fb9d
DakotaScripts <alexmahomebcso.org@gmail.com>
2025-09-06 12:58:24 -0500

    addons
    extras

---
+++ extras/AI-Pit/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+games {"gta5"}
+
+--DO NOT REMOVE THESE
+title "AI Pit Maneuver"
+description "Allows player to pit fleeing AI and they will stop running after performing the pit"
+author "DAKOTASCRIPTS" ----REOWORKED NOT MADE JUST IMPROVED THE AI AND SOME OTHER THINGS
+
+client_scripts {
+    "client.lua",
+}
+
+
+
+----WILLL
+        ----NOT
+            ----SUPPORT
+                    ----IF
+                        ---BROKEN DONT TOUCH ANYTHING DUMBASS
---
+++ extras/FireScript/__resource.lua
@@ -0,0 +1,9 @@
+-- Manifest
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+name 'FireScript'
+description 'Allows you to simulate fires and smoke.'
+author 'DakotaScripts (https://www.youtube.com/albo1125)'
+version 'v1.4.0'
+url 'https://github.com/Albo1125/FireScript'  ---THIS IS REWORKED NOT MADE
+client_script 'FireScript.net.dll'
+server_script 'sv_firescript.lua'
